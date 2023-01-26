54619eddd1f124a63835eb7f12b6ff8ed6dca981
AshDragon55 <ashert1000@gmail.com>
2023-01-26 14:06:55 -0400

    2pm

--- imports/layout.js
+++ imports/layout.js
@@ -1,7 +1,10 @@
+console.warn("layout file")
 import './addanimal.html'
 import './addanimal.js'
 
+import './editanimal.html'
+import './editanimal.js'
+
 import './viewanimals.html'
 import './viewanimals.js'
-
 
--- imports/viewanimals.js
+++ imports/viewanimals.js
@@ -3,3 +3,11 @@ Template.viewanimals.helpers({
         return farmdb.find()
     }
 })
+
+Template.viewanimals.events({
+    'click.js-edit'(){
+        console.warn("Your are editing", this).value = this .type
+        $('#editModal').modal('show')
+
+    }
+})
