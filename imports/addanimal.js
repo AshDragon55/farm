c94c2b39b4caffea0fe7a34d8afd56ddca002386
AshDragon55 <ashert1000@gmail.com>
2023-02-14 11:11:36 -0700

    blank page fix

--- imports/addanimal.js
+++ imports/addanimal.js
@@ -1,17 +1,17 @@
 Template.addAnimal.events({
-    'click .js-saveAnimal'() {
-      console.debug("save button clicked")
-        let type = document.querySelector('.addanimalType').value
-        let legs = document.querySelector('.addanimalLegs').value
-        console.debug(`The ${type} has ${legs} legs`)
-        farmdb.insert({
-            'type':type,
-            'numLegs' : legs
-            
-        })
-    },
-    'click .js-showAdd'() {
-      // let addModal = document.querySelector("#addModal")
-      $('#addModal').modal('show')
-    }
-  })
+  'click .js-saveAnimal'() {
+    console.debug("save button clicked")
+      let type = document.querySelector('.addanimalType').value
+      let legs = document.querySelector('.addanimalLegs').value
+      console.debug(`The ${type} has ${legs} legs`)
+      farmdb.insert({
+          'type':type,
+          'numLegs' : legs
+          
+      })
+  },
+  'click .js-showAdd'() {
+    // let addModal = document.querySelector("#addModal")
+    $('#addModal').modal('show')
+  }
+})
